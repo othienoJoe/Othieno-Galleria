183a205dbd1ca651b775093f07992a9134e390d3
othienoJoe <bryo094@gmail.com>
2021-10-09 21:41:34 +0300

    updated models and settings files

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 # Image model
 class Image(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length = 60)
     description = models.TextField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)

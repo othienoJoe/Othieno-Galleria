5e8a75993ac41d34dc216ca7f96964429951658b
othienoJoe <bryo094@gmail.com>
2021-10-09 20:13:00 +0300

    added urls file in the app folder and also created path in it

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Image
+from django.http import HttpResponse
 
 # Create your views here.
+def welcome(request):
+	return HttpResponse()
+
+# The index function for displaying the images

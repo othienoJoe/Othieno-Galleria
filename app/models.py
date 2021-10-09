0d37c41caa0420393d2160c62c8469f0a0b5b78c
othienoJoe <bryo094@gmail.com>
2021-10-09 21:49:19 +0300

    updated the models file by adding the location and category

--- app/models.py
+++ app/models.py
@@ -2,6 +2,40 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
+# The location model
+class Location(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    # To save location to database
+    def save_location(self):
+        self.save()
+    # Update location
+    def update_location(self, name):
+        self.name = name
+        self.save()
+     # Delete location from database
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+# The category model
+class Category(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    # Save category to database
+    def save_category(self):
+        self.save()
+    # To update category
+    def update_category(self, name):
+        self.name = name
+        self.save()
+    # Delete category from database
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
 # Image model
 class Image(models.Model):
     name = models.CharField(max_length = 60)

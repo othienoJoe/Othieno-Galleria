38fcaa6c8dd4a88a1fef471c9b4fc0ea76408bd1
othienoJoe <bryo094@gmail.com>
2021-10-11 10:28:55 +0300

    installed whitenoise and ran the location tests

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Location, Category, Image
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+from app.models import Location
+
 # Create your tests here.
+class LocationTestCase(TestCase):
+	def setUp(self):
+		Location.objects.create(name="Test Location")
+
+	def test_location_name(self):
+		location = Location.objects.get(name="Test Location")
+		self.assertEqual(location.name, "Test Location")
+
+	def test_location_str(self):
+		location = Location.objects.get(name="Test Location")
+		self.assertEqual(str(location), "Test Location")
+		
